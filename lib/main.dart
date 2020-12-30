3a6273d6bd12e5ac82696aab840aeafa1e96abc2
epic-unicorn <struijs@gmail.com>
2020-12-30 14:46:12 +0100

    first tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_countdown_timer/index.dart';
+import 'package:vibration/vibration.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aftellen',
+      title: 'Aftellen naar 2021',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        primarySwatch: Colors.lightBlue,
       ),
-      home: MyHomePage(title: 'Aftellen'),
+      home: MyHomePage(title: 'Aftellen naar 2021'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  CountdownTimerController controller;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    // int endTime = DateTime.parse("2021-01-01 00:00:00").millisecondsSinceEpoch;
+    int endTime = DateTime.now().millisecondsSinceEpoch + 10000;
+    controller = CountdownTimerController(endTime: endTime, onEnd: onEnd);
+  }
+
+  Future<void> onEnd() async {
+    print('END');
+    if (await Vibration.hasVibrator()) {
+      Vibration.vibrate();
+    }
   }
 
   @override
@@ -46,20 +55,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Pushed:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            CountdownTimer(
+              controller: controller,
+              widgetBuilder: (_, CurrentRemainingTime time) {
+                if (time == null) {
+                  return Text('Game over');
+                }
+                return Text('${time.hours}:${time.min}:${time.sec}',
+                    style: TextStyle(
+                        fontSize: 128.0, fontWeight: FontWeight.bold));
+              },
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
